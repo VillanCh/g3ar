ea2bd6bf2c37960bc32bdb6e4ad6a5515bf07148
v1ll4n <188228725@qq.com>
2017-01-09 00:43:04 +0800

    add ?

--- g3ar/threadutils/thread_pool.py
+++ g3ar/threadutils/thread_pool.py
@@ -10,7 +10,7 @@ import unittest
 #import multiprocessing 
 from pprint import pprint
 from time import sleep
-from queue import Full, Empty, Queue
+from Queue import Full, Empty, Queue
 #from random import choice
 #from traceback import format_exc
 from threading import Thread, Lock
